b5aa705108c79a13d68a43d74e590c93c672b359
Toktamysov Timur <timur.toktamysov@gmail.com>
2024-10-29 23:25:12 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-'t.ih1mHMIYiwF-5WqlWftOX-A5FyuKxjAm128C6GO65sdi9s4rFsEHTQL8eOfwdeMSmUU_SNa3rR0zvOabBaRTWA'
+from tinkoff.invest import Client
+
+TOKEN = "t.ih1mHMIYiwF-5WqlWftOX-A5FyuKxjAm128C6GO65sdi9s4rFsEHTQL8eOfwdeMSmUU_SNa3rR0zvOabBaRTWA"
+
+with Client(TOKEN) as client:
+    print(client.users.get_accounts())
+
+
