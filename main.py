1dc876c336452e54d550a1d2ba5add38136480f1
Toktamysov Timur <timur.toktamysov@gmail.com>
2024-10-29 23:56:07 +0300

    refactoring main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from tinkoff.invest import Client
-
-TOKEN = "t.ih1mHMIYiwF-5WqlWftOX-A5FyuKxjAm128C6GO65sdi9s4rFsEHTQL8eOfwdeMSmUU_SNa3rR0zvOabBaRTWA"
+from importing_modules import *
 
 with Client(TOKEN) as client:
     print(client.users.get_accounts())
